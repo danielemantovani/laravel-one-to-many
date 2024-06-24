90c69f9040a10b6bb475545c252d961af5420d42
danielemantovani <daniele.mantovani124@gmail.com>
2024-06-24 18:32:26 +0200

    feat: type of project in show

--- resources/views/admin/project/show.blade.php
+++ resources/views/admin/project/show.blade.php
@@ -4,6 +4,7 @@
 <div class="container p-5">
     <h2 class="text-center mb-5">{{$project->title}}</h2>
     <p>{{$project->content}}</p>
+    <p>{{$project->type?->technology}}</p>
     <p>{{$project->slug}}</p>
 </div>
 @endsection
