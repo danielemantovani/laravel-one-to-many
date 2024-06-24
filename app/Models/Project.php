33f4a1dd259e945a10d9b04323a264c0f564fe04
danielemantovani <daniele.mantovani124@gmail.com>
2024-06-24 17:40:53 +0200

    feat: add function type in Project model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -10,4 +10,8 @@ class Project extends Model
     use HasFactory;
 
     protected $fillable = ['title', 'content', 'slug'];
+
+    public function type(){
+        return $this->belongsTo(Type::class);
+    }
 }
